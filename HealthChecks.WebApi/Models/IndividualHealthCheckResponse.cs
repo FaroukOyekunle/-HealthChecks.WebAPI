61462ea484065cfc6fd38303fe63c956f00bc126
oyekunle farouk <oyekunlefarouk2001@gmail.com>
2022-12-02 10:42:19 +0100

    Add project files.

---
+++ HealthChecks.WebApi/Models/IndividualHealthCheckResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthChecks.WebApi.Models
+{
+    // Better Healthcheck Reponse.
+    // Let's add a bunch of response classes to generate a more readable and meaningful response.
+    // Response Class for Component-wise Health
+    public class IndividualHealthCheckResponse
+    {
+        public string Status { get; set; }
+        public string Components { get; set; }
+        public string Description { get; set; }
+    }
+}
